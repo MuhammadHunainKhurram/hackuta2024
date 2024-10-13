f0526b808816a9fad5a368ee47e72efaf0c45e8d
Kevin Farokhrouz <kevinafarokhrouz@gmail.com>
2024-10-13 02:30:11 -0500

    Fix build errors

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,8 +1,8 @@
-import { fetchGeneratedCourses } from '@/lib/mongo/fetchGeneratedCourses';
+// import { fetchGeneratedCourses } from '@/lib/mongo/fetchGeneratedCourses';
 import SendCourseButton from '../_components/SendCourseButton';
 
 export default async function Home() {
-  const fetched_courses = await fetchGeneratedCourses();
+  // const fetched_courses = await fetchGeneratedCourses();
 
   return (
     <div>
