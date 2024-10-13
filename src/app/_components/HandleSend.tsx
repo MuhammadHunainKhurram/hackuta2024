756f5d2bb4d963e8278fa9cdc95473990224463c
ethan-ung <ethan.s.ung04pv@gmail.com>
2024-10-13 05:55:59 -0500

    Create HandleSend.tsx

---
+++ src/app/_components/HandleSend.tsx
@@ -0,0 +1,30 @@
+'use client';
+import { useState } from 'react';
+import { CourseData } from '@/lib/mongo/sendGeneratedCourse';
+
+interface SendCourseProps {
+  courseData: CourseData;
+}
+
+export const HandleSend = async ({ courseData }: SendCourseProps) => {
+  try {
+    const response = await fetch('/api/SendCourse', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(courseData),
+    });
+    
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const courseToSend = await response.json();
+    console.log(courseToSend);
+    return courseToSend;
+  } catch (error) {
+    console.error('Failed to send course data:', error);
+    throw error;
+  }
+};
